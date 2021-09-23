2990de4a3b99292d1d2fe21eda9d458968aadbfe
iagoracode <iagorenator@gmail.com>
2021-09-23 15:04:03 -0300

    Bingo em funcionamento e ajustes de responsividade

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let vetorJogadores = [];
+
 function gerarNumeroAleatorio(inicioIntervalo, fimIntervalo) {
     return Math.floor((fimIntervalo - inicioIntervalo) * Math.random()) + inicioIntervalo;
 }
@@ -25,10 +27,25 @@ function gerarCartela(){
 
 function gerarCartelaHTML() {
 
-    let nomeJogador = prompt("Digite o nome do jogador:;")
+    let nomeJogador = prompt("Digite o nome do jogador:");
+    if(nomeJogador == "" || nomeJogador == null){
+        alert("Você precisa digitar o nome do jogador.");
+        return;
+    }else if(nomeJogador.length <= 3){
+        alert("O nome é muito pequeno.");
+        return;
+    }
 
     let titulo = ['B', 'I', 'N', 'G', 'O'];
     let cartela = gerarCartela();
+
+    let jogador = {
+        nome: nomeJogador,
+        cartela: cartela
+    }
+
+    vetorJogadores.push(jogador);
+    console.log(vetorJogadores);
 
     let divBingo = document.getElementById("bingo");
 
@@ -75,3 +92,59 @@ function gerarCartelaHTML() {
     divBingo.appendChild(divCartela);
 
 }
+
+function verificaCartela(cartela, numerosSorteados, quantidadeNumeros) {
+    if (numerosSorteados.length < quantidadeNumeros) {
+        return false;
+    }
+    let numerosExistem = true;
+    cartela.forEach(function (numero) {
+        if (numerosSorteados.includes(numero) == true) {
+            numerosExistem = true;
+        } else {
+            numerosExistem = false;
+            return false;
+        }
+    });
+    if (numerosExistem == true) {
+        return true;
+    }
+}
+
+function jogarBingo() {
+    if(vetorJogadores.length <= 1){
+        alert("Você precisa ter pelo menos dois jogadores para jogar!");
+        return;
+    }
+    let numerosSorteados = [];
+    let divSorteados = document.getElementById("sorteados");
+    let intervalo = setInterval(function () {
+        let numeroExiste = true;
+        while (numeroExiste == true) {
+            let numeroAleatorio = gerarNumeroAleatorio(1, 75);
+            if (numerosSorteados.includes(numeroAleatorio) == true) {
+                numeroExiste = true;
+            } else {
+                numeroExiste = false;
+                numerosSorteados.push(numeroAleatorio);
+                let divNumero = document.createElement("div");
+                divNumero.classList.add("col-2");
+                divNumero.classList.add("sorteado");
+                divNumero.innerText = numeroAleatorio;
+                divSorteados.appendChild(divNumero);
+                console.log("Números sorteados:", numerosSorteados);
+                vetorJogadores.forEach(function (jogador) {
+                    if (verificaCartela(jogador.cartela, numerosSorteados, 25) == true) {
+                        console.log(`${jogador.nome} ganhou o Bingo! Parabéns!!!`);
+                        clearInterval(intervalo);
+                    }
+                });
+            }
+        }
+        if (numerosSorteados.length >= 75) {
+            prompt("Sorteio Finalizado!");
+            clearInterval(intervalo);
+        }
+    }, 50);
+}
+
